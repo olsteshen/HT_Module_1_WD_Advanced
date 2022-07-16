740ce9fe578fb696eeaffb9ac664adddb96f2595
stesho01 <Listo4ek>
2022-07-16 18:49:23 +0300

    Home task M1 - WD Advanced - add details to scenario

--- src/test/java/com/cucumber/junit/hooks/ScreenshotHook.java
+++ src/test/java/com/cucumber/junit/hooks/ScreenshotHook.java
@@ -1,14 +1,13 @@
 package com.cucumber.junit.hooks;
 
 import driver.SingletonDriver;
-import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class ScreenshotHook {
-    @After
+
     public void takeScreenshot(Scenario scenario){
         byte[] screenshot = ((TakesScreenshot) SingletonDriver.getInstance()).getScreenshotAs(OutputType.BYTES);
         scenario.attach(screenshot, "image/png", scenario.getName());
